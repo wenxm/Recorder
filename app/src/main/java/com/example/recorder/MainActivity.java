4f9269af1e9b0fb15f5da508aff39d6becf27259
wenxm <837727935@qq.com>
2016-01-24 19:55:35 +0800

    1

--- app/src/main/java/com/example/recorder/MainActivity.java
+++ app/src/main/java/com/example/recorder/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -292,5 +291,4 @@ public class MainActivity extends Activity implements OnClickListener {
             myPlayer.release();
         }
     }
-
-}  
+}
